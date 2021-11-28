d906832f4949311d2a2f6f5a532e45ea68a8efaa
Qasim Ahmed <35306677+qasim98@users.noreply.github.com>
2021-11-28 15:23:10 -0500

    Fixed some issues with UI and used bootstrap for inputs

--- Digital_Dashboard/Dashboard/forms.py
+++ Digital_Dashboard/Dashboard/forms.py
@@ -1,8 +1,17 @@
 from django.forms import ModelForm
+from django import forms
+from django.forms.widgets import Widget
 from .models import Districts
 
+#Widgets is using boostrap styling
 
 class DistrictForm(ModelForm):
     class Meta:
         model = Districts
         fields = ['state', 'district_id']
+
+        widgets = {
+            'state' : forms.TextInput(attrs={'class' : 'form-control', 'style': 'width: 150px;'}),
+            'district_id' : forms.TextInput(attrs={'class' : 'form-control', 'style': 'width: 150px;'}),
+
+        }
--- Digital_Dashboard/Digital_Dashboard/settings.py
+++ Digital_Dashboard/Digital_Dashboard/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Dashboard',
-    'educator.apps.EducatorConfig'
+    'educator.apps.EducatorConfig',
+    'crispy_forms'
 
 ]
 
