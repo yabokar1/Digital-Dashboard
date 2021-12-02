e52007b9b2e47b03ceb7c13c745302a41a361b27
Qasim Ahmed <35306677+qasim98@users.noreply.github.com>
2021-12-02 00:29:41 -0500

    Added one more graph which shows on front end and fixed layout

--- Digital_Dashboard/Dashboard/views.py
+++ Digital_Dashboard/Dashboard/views.py
@@ -31,8 +31,145 @@ def percentage_access_in_state(state):
     return county_connection,district_list
 
 
+def expenditure_per_pupil_in_different_states():
+    avg_expenditure_for_states = []
+    Utah = []
+    Illi = []
+    Wisco = []
+    mean_perc = []
+    north = []
+    miss = []
+    wash = []
+    connect = []
+    mass = []
+    newyork = []
+    indiana = []
+    vir = []
+    ohio = []
+    jersey = []
+    cal = []
+    dis = []
+    ari = []
+    tex = []
+    states = ['Utah', 'Illinois', 'Wisconsin', 'NC', 'Missouri', 'Washington', 'Connecticut', 'Massachusetts', 'NY', 'Indiana', 'Virginia', 'Ohio', 'New Jersey', 'California', 'DOC', 'Arizona','Texas']
+
+    o = Districts.objects.all()
+
+    for item in o:
+        if item.state == 'Utah':
+            Utah.append(item.pp_total_raw)
+        elif item.state == 'Illinois':
+            Illi.append(item.pp_total_raw)
+        elif item.state == 'Wisconsin':
+            Wisco.append(item.pp_total_raw)
+        elif item.state == 'North Carolina':
+            north.append(item.pp_total_raw)
+        elif item.state == 'Missouri':
+            miss.append(item.pp_total_raw)
+        elif item.state == 'Washington':
+            wash.append(item.pp_total_raw)
+        elif item.state == 'Connecticut':
+            connect.append(item.pp_total_raw)
+        elif item.state == 'Massachusetts':
+            mass.append(item.pp_total_raw)
+        elif item.state == 'New York':
+            newyork.append(item.pp_total_raw)
+        elif item.state == 'Indiana':
+            indiana.append(item.pp_total_raw)
+        elif item.state == 'Virginia':
+            vir.append(item.pp_total_raw)
+        elif item.state == 'Ohio':
+            ohio.append(item.pp_total_raw)
+        elif item.state == 'New Jersey':
+            jersey.append(item.pp_total_raw)
+        elif item.state == 'California':
+            cal.append(item.pp_total_raw)
+        elif item.state == 'District Of Columbia':
+            dis.append(item.pp_total_raw)
+        elif item.state == 'Arizona':
+            ari.append(item.pp_total_raw)
+        elif item.state == 'Texas':
+            tex.append(item.pp_total_raw)
+
+    if len(Utah) > 0:
+        avg_expenditure_for_states.append(avg(Utah))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(Illi) > 0:
+        avg_expenditure_for_states.append(avg(Illi))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(Wisco) > 0:
+        avg_expenditure_for_states.append(avg(Wisco))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(north) > 0:
+        avg_expenditure_for_states.append(avg(north))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(miss) > 0:
+        avg_expenditure_for_states.append(avg(miss))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(wash) > 0:
+        avg_expenditure_for_states.append(avg(wash))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(connect) > 0:
+        avg_expenditure_for_states.append(avg(connect))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(mass) > 0:
+        avg_expenditure_for_states.append(avg(mass))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(newyork) > 0:
+        avg_expenditure_for_states.append(avg(newyork))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(indiana) > 0:
+        avg_expenditure_for_states.append(avg(indiana))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(vir) > 0:
+        avg_expenditure_for_states.append(avg(vir))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(ohio) > 0:
+        avg_expenditure_for_states.append(avg(ohio))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(jersey) > 0:
+        avg_expenditure_for_states.append(avg(jersey))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(cal) > 0:
+        avg_expenditure_for_states.append(avg(cal))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(dis) > 0:
+        avg_expenditure_for_states.append(avg(dis))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(ari) > 0:
+        avg_expenditure_for_states.append(avg(ari))
+    else:
+        avg_expenditure_for_states.append(0)
+    if len(tex) > 0:
+        avg_expenditure_for_states.append(avg(tex))
+    else:
+        avg_expenditure_for_states.append(0)
+
+    final_states_list = json.dumps(states)
+    final_expenditure_list = json.dumps(avg_expenditure_for_states)
+    return final_states_list,final_expenditure_list
 
 
+def avg(li):
+    j = 0
+    for i in li:
+        j = j + i
+    return (j/len(li))
 
 def percentage_access_black_hispanic(request):
     states = ['Utah', 'Illinois', 'Wisconsin', 'NC', 'Missouri', 'Washington', 'Massachusetts', 'NY', 'Indiana',
@@ -56,6 +193,8 @@ def percentage_access_black_hispanic(request):
     dis = []
     ari = []
     tex = []
+    s = []
+    exp = []
 
     obj = Districts.objects.all()
 
@@ -113,7 +252,8 @@ def percentage_access_black_hispanic(request):
 
     final_state_list = json.dumps(states)
     final_mean_perc_list = json.dumps(mean_perc)
-
+    s, exp = expenditure_per_pupil_in_different_states()
+    print("Expenditure is",exp)
     form = create_district_graph()
     county, district = percentage_access_in_state("Illinois")
 
@@ -127,8 +267,10 @@ def percentage_access_black_hispanic(request):
             print("The county is",county)
             print("The district is",district)
 
-            return render(request, 'index.html', {'district': district, 'county': county, 'form': form, 'state':final_state_list , 'perc': final_mean_perc_list})
+            return render(request, 'index.html', {'district': district, 'county': county, 'form': form, 'state':final_state_list , 'perc': final_mean_perc_list , 'st':s, 'ex':exp})
 
     print("The request post is",request.POST)
-    return render(request, 'index.html', {'state':final_state_list , 'perc': final_mean_perc_list, 'form': form})
+    return render(request, 'index.html', {'state':final_state_list , 'perc': final_mean_perc_list, 'form': form, 'st':s, 'ex':exp})
+
+
 
